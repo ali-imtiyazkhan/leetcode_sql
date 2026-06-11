ef974013f15c0961676dd782dead5ddaede6ef7b
imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-06-11 09:39:53 +0530

    Time: 285 ms (82.80%) | Memory: 66.8 MB (79.57%) - LeetSync

---
+++ 3844-number-of-ways-to-assign-edge-weights-i/number-of-ways-to-assign-edge-weights-i.py
@@ -0,0 +1,25 @@
+class Solution:
+    def assignEdgeWeights(self, edges):
+        MOD = 10**9 + 7
+        n = len(edges) + 1
+
+        if n == 1:
+            return 0
+
+        g = [[] for _ in range(n + 1)]
+        for u, v in edges:
+            g[u].append(v)
+            g[v].append(u)
+
+        depth = 0
+        stack = [(1, 0, 0)]  # node, parent, depth
+
+        while stack:
+            node, parent, d = stack.pop()
+            depth = max(depth, d)
+
+            for nei in g[node]:
+                if nei != parent:
+                    stack.append((nei, node, d + 1))
+
+        return pow(2, depth - 1, MOD)
