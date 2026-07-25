8f2634aaccf06bd37a53b6762d4b2a26764460e9
imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-07-25 17:46:28 +0530

    Time: 7 ms (18.08%) | Memory: 23.9 MB (10.48%) - LeetSync

--- 136-single-number/single-number.cpp
+++ 136-single-number/single-number.cpp
@@ -1,10 +1,17 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int ans = 0;
-        for(int n : nums){
-            ans ^= n;
+        unordered_set<int> st;
+
+        for (int num : nums) {
+            if(st.count(num)){
+                st.erase(num);
+            }
+            else{
+                st.insert(num);
+            }
         }
-        return ans;
+
+        return *st.begin();
     }
 };
