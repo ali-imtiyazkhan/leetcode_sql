8b4c06c029d40311b164447312f79e12dddcb551
imtiyaz A. Khan <itzkhantijara@gmail.com>
2026-09-18 19:26:42 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (23.53%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
 
-        int n = nums.size();
-
-        if (n == 1)
-            return nums[0];
-
-        vector<int> dp(n);
-
-        dp[0] = nums[0];
-        dp[1] = max(nums[0], nums[1]);
-
-        for (int i = 2; i < n; i++) {
-            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1]);
+    int solve(vector<int>& nums, int index, vector<int>& dp) {
+        if(index >= nums.size()) {
+            return 0;
         }
 
-        return dp[n - 1];
+        if(dp[index] != -1) {
+            return dp[index];
+        }
+        
+    //  ek case solve karo baki recursion solve kar dega 
+        int include = nums[index] + solve(nums, index + 2, dp);
+
+        int exclude = solve(nums, index + 1, dp);
+
+        return dp[index] = max(include, exclude);
+    }
+
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size(), -1);
+
+        return solve(nums, 0, dp);
     }
 };
